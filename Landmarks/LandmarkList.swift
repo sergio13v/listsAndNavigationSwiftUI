d3b7d9f2f8c6b472ed9a44f34b704720a16f4513
sergio13v <56689687+sergio13v@users.noreply.github.com>
2019-12-01 05:23:55 +0100

    Links finished

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -10,12 +10,19 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        NavigationView {
+            List(landmarkData) {
+                landmark in    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+            }
+.navigationBarTitle(Text("Landmarks"))
+        }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
     }
+}
 }
